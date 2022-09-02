8c57de9d083cffcfe0584745b8e9b06b55ea2ceb
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-09-02 10:21:34 +0530

    long-task

--- callback.js
+++ callback.js
@@ -1,7 +1,16 @@
-const dt = new Date();
-console.log("hello");
-for (i = 0; i <= 10; i++) {
-  console.log("looping......");
+// const dt = new Date();
+// console.log("hello");
+// for (i = 0; i <= 10; i++) {
+//   console.log("looping......");
+// }
+// const diff = new Date() - dt;
+// console.log(diff);
+
+function longTask(millSecondTime) {
+  const dt = new Date();
+  while (new Date() - dt <= millSecondTime) {}
 }
-const diff = new Date() - dt;
-console.log(diff);
+
+console.log("start.....");
+longTask(2000);
+console.log("end.....");
