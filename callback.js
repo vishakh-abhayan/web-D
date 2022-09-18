3c2ef225c66ddeaeb353fb6a3dd36c06b8f0eb7e
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-09-18 13:56:31 +0530

    object litaral

--- callback.js
+++ callback.js
@@ -6,20 +6,34 @@
 // const diff = new Date() - dt;
 // console.log(diff);
 
-function longTask(millSecondTime) {
-  const dt = new Date();
-  while (new Date() - dt <= millSecondTime) {}
+// function longTask(millSecondTime) {
+//   const dt = new Date();
+//   while (new Date() - dt <= millSecondTime) {}
+// }
+
+// function showEnd() {
+//   console.log("end.....");
+// }
+
+// console.log("start.....");
+// setTimeout(showEnd, 2000);
+
+// console.log("start.....");
+// setTimeout(showEnd, 2000);
+
+// console.log("start.....");
+// setTimeout(showEnd, 2000);
+
+let person = {
+  name: "vishakh",
+  age: "18",
+  display: function () {
+    console.log(this.name);
+  },
+};
+
+for (x in person) {
+  console.log(person[x]);
 }
 
-function showEnd() {
-  console.log("end.....");
-}
-
-console.log("start.....");
-setTimeout(showEnd, 2000);
-
-console.log("start.....");
-setTimeout(showEnd, 2000);
-
-console.log("start.....");
-setTimeout(showEnd, 2000);
+person.display();
