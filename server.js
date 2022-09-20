5b6147948cf49c98ae3d5bd5a7d05d91e00c0e3d
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-09-20 19:12:18 +0530

    server responds

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 let http = require("http");
+let fs = require("fs");
 
-http.createServer(hey).listen(3000);
+http
+  .createServer(function (req, res) {
+    fs.readFile("index.html", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  })
+  .listen(3000);
 
-function hey(req, res) {
-  res.write("User Found!");
-  res.end();
-}
+// function (req, res) {
+//   res.write("User Found!");
+//   res.end();
+// }
