be79cd7f1ad4807536c6f2a9e4e5cac1c7e1bc81
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-09-21 11:58:13 +0530

    s inete tml

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ let fs = require("fs");
 
 http
   .createServer(function (req, res) {
-    fs.readFile("index.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
+    if (req.url == "/") {
+      fs.readFile("index.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else if (req.url == "/login") {
+      fs.readFile("login.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write("error");
       res.end();
-    });
+    }
   })
-  .listen(3000);
+  .listen(3000, () => console.log("server runing..........."));
 
 // function (req, res) {
 //   res.write("User Found!");
