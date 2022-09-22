2be4f9542c45478901ccb633319a0ba687dff0a1
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-09-22 13:00:15 +0530

    singup form crt

--- express.js
+++ express.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Helllo world!...."));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
+app.get("/about", (req, res) => {
+  res.send("welocom world!");
+});
 
 app.listen(8000, () => console.log("server started............"));
