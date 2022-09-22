0f354aae81ac53e1f55f61f30e2b9f18bfcd810a
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-09-22 13:52:52 +0530

    use comment

--- express.js
+++ express.js
@@ -2,21 +2,22 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("start...");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("start...");
+//   next();
+// });
 
-app.get("/login", (req, res, next) => {
+app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "login.html"));
-  console.log("middle");
-  next();
+//   console.log("middle");
+//   next();
 });
-app.use((req, res) => {
-  console.log("end...");
-});
+// app.use((req, res) => {
+//   console.log("end...");
+// });
 app.post("/login", (req, res) => {
   res.send("login sucessfully");
+  res.
 });
 app.get("/", (req, res) => {
   res.send("welocom world!");
