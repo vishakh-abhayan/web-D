db6c5575bab1f2cd2c053724c528ac1857228fe0
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-09-24 21:52:03 +0530

    cst mildiware

--- debugEnv.js
+++ debugEnv.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", Token, (req, res) => {
   res.send("<h1>Hey Vishakh </h1>");
 });
+
+function Token(req, res, next) {
+  console.log("token created.........");
+  next();
+}
 
 app.get("*", (req, res) => {
   res.status(404).send("<h1> 404 </h1>");
