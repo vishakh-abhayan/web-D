5700e7210599251635e93de24dd6714b066717c4
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-10-09 12:31:20 +0530

    promis

---
+++ promis.js
@@ -0,0 +1,29 @@
+function add(num1, num2, callback) {
+  let err = false;
+  if (num1 === 0) {
+    err = true;
+  }
+  callback(num1 + num2, err);
+}
+
+function multi(num1, num2, callback) {
+  callback(num1 * num2);
+}
+
+function div(num1, num2, callback) {
+  callback(num1 / num2);
+}
+
+add(50, 40, (sum, err) => {
+  if (err) {
+    console.log("first number is zero");
+  } else {
+    console.log(sum);
+    multi(sum, sum, (product) => {
+      console.log(product);
+      div(product, 50, (result) => {
+        console.log(result);
+      });
+    });
+  }
+});
